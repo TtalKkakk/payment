8f347eff13f2c81d56d96076e3eaa7c9aab94f3d
Jinwook Lee <gfdsa9497@naver.com>
2026-03-28 01:40:08 +0900

    :bug: fix : CardCompany 도메인 Adapter 수정

--- src/main/java/com/example/pg/card_company/application/adapter/CardCompanyAdapter.java
+++ src/main/java/com/example/pg/card_company/application/adapter/CardCompanyAdapter.java
@@ -1,8 +1,11 @@
 package com.example.pg.card_company.application.adapter;
 
-import com.example.pg.card_company.application.CardCompanyService;
 import com.example.pg.card_company.domain.aggergate.CardCompany;
+import com.example.pg.card_company.domain.vo.CardCompanyCode;
+import com.example.pg.card_company.infrastructure.persistence.CardCompanyRepository;
 import com.example.pg.card_company.presentation.port.CardCompanyPort;
+import com.example.pg.common.exception.BusinessException;
+import com.example.pg.common.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -11,9 +14,11 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class CardCompanyAdapter implements CardCompanyPort {
-    private final CardCompanyService cardCompanyService;
+    private final CardCompanyRepository cardCompanyRepository;
     @Override
     public CardCompany getCardCompanyByCode(String code) {
-        return cardCompanyService.findByCode(code);
+        log.debug("[CardCompany] Query findByCode cardCompanyCode={}", code);
+        return cardCompanyRepository.findByCode(new CardCompanyCode(code))
+                .orElseThrow(() -> new BusinessException(ErrorCode.CARD_COMPANY_NOT_FOUND, code));
     }
 }
