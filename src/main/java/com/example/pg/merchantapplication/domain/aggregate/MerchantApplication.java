6114bc338c1bf4f7918ed565c76adf577a12af6e
Jinwook Lee <gfdsa9497@naver.com>
2026-03-31 23:52:08 +0900

    :bug: fix : MerchantApplications 도메인 version 필드 nullable=false 추가

--- src/main/java/com/example/pg/merchantapplication/domain/aggregate/MerchantApplication.java
+++ src/main/java/com/example/pg/merchantapplication/domain/aggregate/MerchantApplication.java
@@ -79,7 +79,7 @@ public class MerchantApplication {
 
     @Getter
     @Version
-    @Column(name = "version")
+    @Column(name = "version", nullable = false)
     private Long version;
 
     public MerchantApplication(
